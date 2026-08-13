5d42c65e9ad9a1876d473886685ceefdf7d31f5c
Arun <kundurarunreddy2658@gmail.com>
2026-08-13 14:48:14 +0530

    hour 4 update

---
+++ space.js
@@ -0,0 +1,70 @@
+const planets = {
+
+    Mercury: {
+        description:
+        "The smallest planet and the closest planet to the sun." ,
+
+        temperature:
+            "167&deg;C",
+
+            population:
+                "0",
+
+            distance:
+                "57.9 Million km",
+
+            moons:
+                "0",
+
+            orbit:
+                "88 Days",
+
+            gravity:
+                "3.70 m/s&sup2;",
+            
+
+            facts: [
+                "Mercury is the smallest planet.",
+                "A year on Mercury lasts only 88 Earth days.",
+                "Mercury has almost no atmosphere.",
+                "It explerience extreme temperature change."
+
+            ]
+        
+
+    },
+
+    Venus: {
+
+        description:
+            "A hot world covered by thick clouds and a powerful greenhouse effect.",
+
+        temperature:
+            "464&degC",
+
+        population:
+            "0",
+
+        distance:
+            "108.2 Millions km",
+        
+        moons:
+            "0",
+
+        orbit:
+            "225 Days",
+
+        gravity:
+            "8.87 m/s&sup;",
+
+        facts: [
+            "Venus is the hottest planet.",
+            "Its atmosphere is mostly carbon dioxide.",
+            "Venus rotates in the opposite diretion to most planets.",
+            "A Venusian day is longer than its year."
+
+        ]
+
+    },
+    
+}
